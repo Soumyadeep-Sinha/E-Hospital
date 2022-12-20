b811694076d4e5f9ed47da546f88ed1b368501ae
Soumyadeep-Sinha <soumyadeep8402@gmail.com>
2022-12-20 23:47:01 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,43 @@
+Project created BY Soumyadeep Sinha
+
+PLEASE READ THE CONTENTS IN ORDER TO AVOID ERRORS
+
+Thank you for visiting my page.
+This is a hospital management project based on MongoDB and Mongoose
+Tools used :
+1) Node Js
+2) Express Js
+3) EJS
+4) HTML
+5) CSS
+6) NPM --> express, body-parser, ejs, mongoose
+
+NOTE : The project is meant to run on Localhost:300 by default.
+but can be configured to run on cloud MongoDB Atlas Servers.
+
+Functionalities :
+
+--> User can create an account
+--> login using the credentials
+--> view or update information 
+--> deregester (delete account)
+
+--> User can register any patient in any department (currently : in Anesthesiology and cardiology)
+--> then they can print receipt 
+--> if required they can cancel the registration.
+
+Features :
+--> Dynamic Home page 
+--> Dynamic Patient Registration Page.
+--> Full CRUD functions to the USER.
+--> Create and Delete function for patient registration.
+--> Pages can be translated in any Language with the help of google translate API
+--> One click print function on receipt page.
+
+Things to take care : 
+
+--> Navigation bar (header) is not fuctional (I plan to change the navbar style in future). It is just a dummy.
+--> additional Node modules might be needed.
+--> The pages are not mobile optimized (Front-end was not my priority)
+--> I may fix the rsponsiveness in future.
+--> The app.js has some console logs wich are unecessary but I have kept them for debugging purposes.
---
+++ recovery.js
@@ -0,0 +1,411 @@
+/*imporing node modules*/
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+/*import ends*/
+
+/*initializing the modules*/
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+/*ending initialization*/
+
+/*declaring server urls*/
+const uri = "mongodb+srv://" + process.env.NAME + ":" + process.env.PASSWORD + "08@hospital-cluster.gbtkhxf.mongodb.net/?retryWrites=true&w=majority";
+/*console.log(uri);*/
+const locale = "mongodb://localhost:27017/hospitalDB";
+/*depriciation warning suppressed*/
+mongoose.set('strictQuery', true);
+/*done declaring urls*/
+
+/*connecting mongoose*/
+mongoose.connect(locale, function (err) {
+    if (err) {
+        console.log("connection error");
+        console.log(err);
+        mongoose.disconnect();
+    } else {
+        console.log("successfully connected to your MongoDB database.");
+    }
+});
+/*connection function ends*/
+
+/*creating routes*/
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.get("/register", function (req, res) {
+    res.sendFile(__dirname + "/pages/user_register.html");
+});
+
+app.get("/login", function (req, res) {
+    res.sendFile(__dirname + "/pages/login.html");
+});
+
+app.get("/home", function (req, res) {
+    res.sendFile(__dirname + "/pages/home.html");
+});
+
+app.get("/update", function (req, res) {
+    res.sendFile(__dirname + "/pages/acoount_update_page.html");
+});
+
+/*declaring mongoose schema for user registration*/
+
+const userdata = new mongoose.Schema({
+    Name: { type: String, required: true },
+    Gender: { type: String, required: true },
+    DOB: { type: Date, required: true },
+    Phone_No: { type: String, required: true, unique: true },
+    Email: { type: String, required: true, unique: true },
+    Password: { type: String, required: true }
+});
+
+/*initializing a model*/
+const userinputs = new mongoose.model("StoredDatas", userdata);
+
+/*main input function*/
+app.post("/register", async function (req, res) {
+    const maindata = new userinputs({
+        Name: req.body.full_name,
+        Gender: req.body.gender,
+        DOB: req.body.date_of_birth,
+        Phone_No: req.body.mobile_no,
+        Email: req.body.email,
+        Password: req.body.usr_password
+    });
+    let pwd1 = req.body.usr_password;
+    console.log(pwd1)
+    let pwd2 = req.body.usr_password_repeat;
+    console.log(pwd2)
+    if (pwd1 === pwd2) {
+        await maindata.save(function (err) {
+            if (err) {
+                var error_name = "User already registered";
+                res.render("Error_Page", { errmessage: error_name })
+                console.log(err);
+            } else {
+                res.redirect("/login");
+                console.log("user data saved successfully");
+            }
+        });
+    }
+    else {
+        var error_name = "Passwords do not match";
+        res.render("Error_Page", { errmessage: error_name })
+        console.log("password did not match")
+    }
+    console.log("hello")
+});
+
+/*sign in function*/
+app.post("/login", async function (req, res) {
+    try {
+        const email = req.body.Email;
+        const password = req.body.cust_password;
+        const usermail = await userinputs.findOne({ Email: email });
+        /*console.log(email);
+        console.log(password);
+        console.log(usermail);
+        console.log(usermail.Password);*/
+        var id = usermail._id.toString().replace(/ObjectId\("(.*)"\)/, "$1");
+        if (usermail.Password === password) {
+            console.log("success");
+            res.render("home", { theid: id })
+            /*res.send("<h2>you are signed in, your unique user ID is : " + id+"</h2>");*/
+        } else {
+            var err_name = "Sign-in failed. (invalid credentials!). You can go back and try again."
+            res.render("Error_Page", { errmessage: err_name })
+            /*res.send("Oops! invalid credentials,go back and try again");*/
+            console.log("invalid credentials")
+        }
+
+    } catch (error) {
+        var error_name = "looks like you are not registered yet"
+        res.render("Error_Page", { errmessage: error_name })
+        console.log("Not registered");
+    }
+});
+
+/*creating USER CRUD FUNCTIONS*/
+app.get("/user/:id", function (req, res) {
+    console.log(req.params.id)
+    userinputs.findById(req.params.id, function (err, user) {
+        if (err) {
+            res.render("Error_Page", { errmessage: "RECORD NOT FOUND" });
+            /*console.log(err);*/
+        } else {
+            /*console.log(user);*/
+            console.log("First function call : ", user);
+        }
+        res.render("user_info", { Users: user });
+    });
+});
+
+const updateuser = mongoose.model("StoredDatas", userdata);
+
+app.post("/update", function (req, res) {
+    const maindata = new updateuser({
+        _id: req.body.upd_uid,
+        Name: req.body.new_name,
+        Phone_No: req.body.new_phone,
+        Email: req.body.new_email,
+        Password: req.body.new_pwd
+    });
+    console.log("we have id ==>",req.body.upd_uid)
+    updateuser.findByIdAndUpdate(req.body.upd_uid, maindata, function (err, user) {
+        if (err) {
+            res.render("Error_Page", { errmessage: "RECORD NOT FOUND" });
+        } else {
+            console.log("newname ==>", req.body.new_name)
+            res.render("messagepage", { errmessage: "Account Updated", location:"login" });
+        }
+    });
+    
+});
+
+app.get("/user/delete/:id", function (req, res) {
+    console.log(req.params.id)
+    userinputs.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.render("Error_Page", { errmessage: "RECORD NOT FOUND" });
+            /*console.log(err);*/
+        } else {
+            /*console.log(user);*/
+            res.render("messagepage", { errmessage: "Account Deleted",location:"register" })
+        }
+    });
+});
+
+
+
+/*get routes for every department */
+app.get("/Anesthesiology", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Anesthesiology department",
+        nameofimg: "anes",
+        nameofroute: "/Anesthesiology"
+    });
+});
+app.get("/Cardiology", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Cardiology department",
+        nameofimg: "cardiology",
+        nameofroute: "/Cardiology"
+    });
+});
+app.get("/ENT", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "ENT department",
+        nameofimg: "ENT",
+        nameofroute: "ENT",
+        nameofroute: "/ENT"
+    });
+});
+app.get("/Gastroenterology", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Gastroenterology department",
+        nameofimg: "gastro",
+        nameofroute: "/Gastroenterology"
+    });
+});
+app.get("/General", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "General surgery",
+        nameofimg: "generalsurgery",
+        nameofroute: "/General"
+    });
+});
+app.get("/Gynaecology", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Gynaecology department",
+        nameofimg: "gynec",
+        nameofroute: "/Gynaecology"
+    });
+});
+app.get("/Pediatrics", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Pediatrics department",
+        nameofimg: "pedia",
+        nameofroute: "/Pediatrics"
+    });
+});
+app.get("/Neurology", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Neurology department",
+        nameofimg: "neurology",
+        nameofroute: "/Neurology"
+    });
+});
+app.get("/Orthopaedic", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Orthopaedic department",
+        nameofimg: "ortho",
+        nameofroute: "/Orthopaedic"
+    });
+});
+app.get("/Urology", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Urology department",
+        nameofimg: "urology",
+        nameofroute: "/Urology"
+    });
+});
+app.get("/Psychiatry", function (req, res) {
+    res.render("Patient_registration", {
+        nameofdept: "Psychiatry department",
+        nameofimg: "pshycology",
+        nameofroute: "/Psychiatry"
+    });
+});
+/*end of routes*/
+
+app.get("/Success", function (req, res) {
+    res.render("patient_reg_success");
+});
+
+
+
+/*schema for patients*/
+const patient_inputs = new mongoose.Schema({
+    Department: { type: String, required: true },
+    Patient_Name: { type: String, required: true },
+    Patient_Age: { type: Number, required: true },
+    Gender: { type: String, required: true },
+    Contact_No: { type: String, required: true },
+    Date: { type: Date, required: true },
+    Realation: { type: String }
+});
+
+/*new models for each department*/
+const anesthesiainputs = new mongoose.model("AnesthesiologyDatas", patient_inputs);
+/*posting registrations*/
+app.post("/Anesthesiology", async function (req, res) {
+    const patientdata = new anesthesiainputs({
+        Department: "Anesthesiology Department",
+        Patient_Name: req.body.Patient_name,
+        Patient_Age: req.body.Patient_age,
+        Gender: req.body.gender,
+        Contact_No: req.body.Contact_num,
+        Date: req.body.App_date,
+        Realation: req.body.Patient_Rel
+    });
+    await patientdata.save(async function (err) {
+        if (err) {
+            var error_name = "Patient already registered in same department";
+            res.render("Error_Page", { errmessage: error_name })
+            console.log(err);
+        } else {
+            /*res.redirect("/login");*/
+            const contact = req.body.Contact_num;
+            const found = await anesthesiainputs.findOne({ Contact_No: contact });
+            var id = found._id.toString().replace(/ObjectId\("(.*)"\)/, "$1");
+            console.log(patientdata)
+            console.log("Patient registered successfully");
+            console.log(id);
+            res.render("patient_reg_success", { patid: id, routename: "Anesthesiology" })
+        }
+    })
+})
+
+app.get("/Anesthesiology/:id", function (req, res) {
+    console.log(req.params.id)
+    anesthesiainputs.findById(req.params.id, function (err, user) {
+        if (err) {
+            res.render("Error_Page", { errmessage: "RECORD NOT FOUND" });
+            /*console.log(err);*/
+        } else {
+            /*console.log(user);*/
+            res.render("receipt", { users: user, routename: "Anesthesiology" });
+        }
+    });
+});
+app.get("/Anesthesiology/delete/:id", function (req, res) {
+    console.log(req.params.id)
+    anesthesiainputs.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.render("Error_Page", { errmessage: "RECORD NOT FOUND" });
+            /*console.log(err);*/
+        } else {
+            /*console.log(user);*/
+            res.render("messagepage", { errmessage: "REGISTRATION CANCELLED" })
+        }
+    });
+});
+
+/* cardiology */
+const cardioinputs = new mongoose.model("CardiologyDatas", patient_inputs);
+/*posting registrations*/
+app.post("/Cardiology", async function (req, res) {
+    const patientdata = new cardioinputs({
+        Department: "Cardiology Department",
+        Patient_Name: req.body.Patient_name,
+        Patient_Age: req.body.Patient_age,
+        Gender: req.body.gender,
+        Contact_No: req.body.Contact_num,
+        Date: req.body.App_date,
+        Realation: req.body.Patient_Rel
+    });
+    await patientdata.save(async function (err) {
+        if (err) {
+            var error_name = "Patient already registered in same department";
+            res.render("Error_Page", { errmessage: error_name })
+            console.log(err);
+        } else {
+            /*res.redirect("/login");*/
+            const contact = req.body.Contact_num;
+            const found = await cardioinputs.findOne({ Contact_No: contact });
+            var id = found._id.toString().replace(/ObjectId\("(.*)"\)/, "$1");
+            console.log(patientdata)
+            console.log("Patient registered successfully");
+            console.log(id);
+            res.render("patient_reg_success", { patid: id, routename: "Cardiology" })
+        }
+    })
+})
+
+app.get("/Cardiology/:id", function (req, res) {
+    console.log(req.params.id)
+    cardioinputs.findById(req.params.id, function (err, user) {
+        if (err) {
+            res.render("Error_Page", { errmessage: "RECORD NOT FOUND" });
+            /*console.log(err);*/
+        } else {
+            /*console.log(user);*/
+            res.render("receipt", { users: user, routename: "Cardiology" });
+        }
+    });
+});
+app.get("/Cardiology/delete/:id", function (req, res) {
+    console.log(req.params.id)
+    cardioinputs.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.render("Error_Page", { errmessage: "RECORD NOT FOUND" });
+            /*console.log(err);*/
+        } else {
+            /*console.log(user);*/
+            res.render("messagepage", { errmessage: "REGISTRATION CANCELLED" })
+        }
+    });
+});
+
+/*similarly other departments can be created*/
+
+
+
+/*server starter*/
+app.listen(3000, function () {
+    console.log("Server has started successfully on port 3000");
+});
+/*end of function*/
+
+
+
+
+
+
